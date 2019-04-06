e64257ca10f7b1ac40f4c0d0213d5e713ab919ec
marc monjon <sysmarc13@hotmail.com>
2019-04-07 01:48:23 +0200

    Iniciando Furnace Kata

---
+++ Furnace/src/test/java/org/lasencinas/Furnace/AppTest.java
@@ -0,0 +1,27 @@
+package org.lasencinas.Furnace;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class AppTest 
+    extends TestCase
+{
+
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+  
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+	}
+
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
